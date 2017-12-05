70f9225400c27faedf1179115a6c01113d229803
Dan Danilyuk and Michael Brooks <dan@>
2017-12-05 09:58:13 -0800

    remove api call

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
     @products = Product.all
     @order_item = current_order.order_items.new
     # API CALL
-    @response = RestClient.get('https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers= {api_key: ENV['NYT_API_KEY']})
-    binding.pry
+    # @response = RestClient.get('https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers= {api_key: ENV['NYT_API_KEY']})
+    # binding.pry
   end
 end
