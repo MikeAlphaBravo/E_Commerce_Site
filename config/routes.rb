480b85aa84a5235dd6464ba4cc3b583df7564b72
Dan Danilyuk and Michael Brooks <michael@>
2017-12-05 16:42:39 -0800

    fix account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :order_items
   resources :orders
